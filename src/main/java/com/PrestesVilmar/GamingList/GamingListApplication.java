15147216390cedc9349cb0cb6bd2aa93c5dcec20
Vilmar Prestes <prestesvilmar20@gmail.com>
2024-11-15 23:35:41 -0300

    Started

---
+++ src/main/java/com/PrestesVilmar/GamingList/GamingListApplication.java
@@ -0,0 +1,13 @@
+package com.PrestesVilmar.GamingList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingListApplication.class, args);
+	}
+
+}
